136eb4d4b4d5662f39b1b8fe5654fc58d3e17d7e
mtengbot <eng.bot@moderntreasury.com>
2026-08-28 00:03:16 +0000

    seal

--- virtualaccount_test.go
+++ virtualaccount_test.go
@@ -63,6 +63,7 @@ func TestVirtualAccountNewWithOptionalParams(t *testing.T) {
 			RoutingNumberType: moderntreasury.F(moderntreasury.VirtualAccountNewParamsRoutingDetailsRoutingNumberTypeAba),
 			PaymentType:       moderntreasury.F(moderntreasury.VirtualAccountNewParamsRoutingDetailsPaymentTypeACH),
 		}}),
+		VirtualAccountSettingID: moderntreasury.F("182bd5e5-6e1a-4fe4-a799-aa6d9a6ab26e"),
 	})
 	if err != nil {
 		var apierr *moderntreasury.Error
